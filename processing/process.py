6c3dd472fd67cd4990f25c75688089af11caa9fd
peter-fogg <peter.fogg@oberlin.edu>
2013-04-24 17:15:15 -0400

    Added attribute-getting script.

---
+++ processing/process.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import collections
+import sqlite3
+
+DB_FILENAME = './fivethoudb.sql'
+
+connection = sqlite3.connect(DB_FILENAME)
+
+cursor = connection.cursor()
+
+cursor.execute('select * from reviews')
+
+item = cursor.fetchone()
+
+counter = collections.Counter()
+
+i = 0
+while item is not None:
+    i += 1
+    if i % 100000 == 0:
+        print('At number: %s' % i)
+    text = map(lambda s: s.lower().strip('.;,:!"?\'(){}[]'), item[3].split())
+    for word in text:
+        counter[word] += 1
+    item = cursor.fetchone()
+
+import pprint
+pprint.pprint(counter.most_common(400))
